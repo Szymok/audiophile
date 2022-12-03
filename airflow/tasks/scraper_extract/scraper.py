c3cf32ad2da615642d7bd5b3c48a65e867b1745b
Szymon Kowalewski <37596854+Szymok@users.noreply.github.com>
2022-12-03 23:17:41 +0000

    create clean headers function

--- airflow/tasks/scraper_extract/scraper.py
+++ airflow/tasks/scraper_extract/scraper.py
@@ -16,4 +16,36 @@ with open('logs.log', 'w'):
 
 class Scraper:
 	'''
+ 	Encapsulates all the logic for scraper
  	'''
+
+	def __init__(self) -> None:
+		self.base_url = 'https://crinacle.com/ranking'
+
+	def clean_headers(self, headers: list) -> list:
+		'''
+		Formats the table headers in snake case, code friendly format
+
+ 		Args:
+	 		headers (list): Contains the unformatted dirty table headers
+
+		Returns:
+			list: Returns properly, well formatted table headers
+		'''
+		clean_headers = []
+
+		for header in headers:
+			# Remove unnecesary terms
+			if "(" in header or "/" in header:
+			header = header.split(" ")[0]
+
+			# Rename header for conformity between both IEMS and headphones
+			if "Setup" == header:
+				header = "driver_type"
+
+			# Convert to snake case
+			clean.headers.append(
+				re.sub(r"(?<=[a-z])(?=[A-Z])|[^a-zA-Z]", " ", header).replace(" ", "_").lower()
+			)
+
+		return clean_headers
